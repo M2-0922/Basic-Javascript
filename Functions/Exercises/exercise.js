563cc1758f440221849b07a7ff988b4bca0e7545
cysnova <cysnova@gmail.com>
2022-09-13 13:27:27 -0700

    finished the exercise

---
+++ Functions/Exercises/exercise.js
@@ -0,0 +1,16 @@
+function findLargestNumber(number1, number2, number3) {
+    if (number1 > number2) {
+        if (number1 > number3){
+            return number1;
+        }else{
+            return number3;
+        };
+    }else{
+        if (number2 > number3){
+           return number2;
+        }else{
+            return number3;
+        };
+    };
+};
+console.log(findLargestNumber(3,2,1));
