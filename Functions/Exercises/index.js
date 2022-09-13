235b4bd79a353f60fff17ed52c6b50293fd84f60
Kanako Kawakami <beruchan.kawaii420@gmail.com>
2022-09-13 13:18:12 -0700

    first commit

---
+++ Functions/Exercises/index.js
@@ -0,0 +1,32 @@
+function findRectangleArea(width, length) {
+    let RectangleArea = width * length;
+    return RectanfleArea;
+};
+
+console.log(isEvenOrOdd(7));
+
+function findLargestNumber(number1, number2, number3) {
+    if (number1 > number2) {
+        if (number1 > number3){
+            return number1;
+        }else{
+            return number3;
+        };
+    }else{
+        if (number2 > number3){
+            return number2;
+        }else{ 
+            return number3;
+        };
+    };
+};
+
+function isEvenOrOdd(number) {
+    if (number % 2 == 0){
+        return  number + ` is even number`
+    }else{
+        return  number + ` is odd number`
+    };
+};
+
+
